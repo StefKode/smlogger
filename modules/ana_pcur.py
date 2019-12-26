02820cc68ba0c2a38cadb1eb4655e2d1807e2a27
Stefan Koch <11819754+StefKode@users.noreply.github.com>
2019-12-26 11:27:36 +0100

    correct window average

--- modules/ana_pcur.py
+++ modules/ana_pcur.py
@@ -7,21 +7,22 @@ class AnaPCur(AnaGeneric):
         self._update_period = self._opts["update_period"]
         self._val_window = [0] * self._avg_win_size
         self._last_ts = self._get_ts()
+        self._value = None
         
 
     def _update(self):
         self._window_insert(self._inval)
+        self._value = self._window_avg()
         ts = self._get_ts()
         if (ts - self._last_ts) >= self._update_period:
-            self._red.set(self._red_key, self._window_avg())
+            self._red.set(self._red_key, self._value)
             self._last_ts = ts
+
 
     def _window_insert(self, v):
         size = self._avg_win_size
-
         for i in range(0, size - 1):
             self._val_window[i] = self._val_window[i + 1]
-
         self._val_window[size - 1] = v
 
 
@@ -30,7 +31,7 @@ class AnaPCur(AnaGeneric):
         avg  = 0
         for i in range(0, size):
             avg += self._val_window[i]
-        return int(avg / size)
+        return avg / size
 
 
 # vim: set expandtab ts=4:
