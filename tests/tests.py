a2fdc1ec0fd1f102fe72d3b1a521e0624ade4d9e
Stefan Koch <11819754+StefKode@users.noreply.github.com>
2019-12-26 15:40:01 +0100

    improve test reporting

--- tests/tests.py
+++ tests/tests.py
@@ -1,31 +1,49 @@
 from colorama import Fore, Back, Style
+import sys
 
-_cAN = Fore.BLUE  + Style.BRIGHT 
-_cOK = Fore.GREEN + Style.BRIGHT
-_cER = Fore.RED   + Style.BRIGHT
-_RST = Style.RESET_ALL
+try:
+    open(".bold-output").close()
+    bold_out = True
+except:
+    bold_out = False
+
+if bold_out:
+    _cAN = Fore.BLUE  + Style.BRIGHT 
+    _cOK = Fore.GREEN + Style.BRIGHT
+    _cER = Fore.RED   + Style.BRIGHT
+    _RST = Style.RESET_ALL
+else:
+    _cAN = ""
+    _cOK = ""
+    _cER = ""
+    _RST = ""
 
 _count = 0
 
 def announceTest(name):
     global _count
-    print(_cAN + "[%2d] TESTCASE: %s%s" % (_count, name, _RST))
     _count += 1
+    print(_cAN + "[%2d] %s%s" % (_count, name, _RST))
     
 
 def assertEqual(a,b, descr=None, failtext=None):
     if a == b:
-        if descr is not None:
-            print(_cOK + "[%2d] Info: %s" % (_count, str(descr)))
-        print(_cOK + "[%2d] A = %s"       % (_count, str(a)))
-        print(       "[%2d] B = %s"       % (_count, str(b)))
-        print(       "[%2d] Test PASS %s" % (_count, _RST))
+        if bold_out:
+            if descr is not None:
+                print(_cOK + "[%2d] Info: %s" % (_count, str(descr)))
+            print(_cOK + "[%2d] A = %s"       % (_count, str(a)))
+            print(       "[%2d] B = %s"       % (_count, str(b)))
+
+        print(       "[%2d] PASS %s" % (_count, _RST))
     else:
-        if descr is not None:
-            print(_cER + "[%2d] Info: %s" % (_count, str(descr)))
-        print(_cER +     "[%2d] A = %s"   % (_count, str(a)))
-        print(           "[%2d] B = %s"   % (_count, str(b)))
+        if bold_out:
+            if descr is not None:
+                print(_cER + "[%2d] Info: %s" % (_count, str(descr)))
+            print(_cER +     "[%2d] A = %s"   % (_count, str(a)))
+            print(           "[%2d] B = %s"   % (_count, str(b)))
 
         if failtext is not None:
             print("%s" % str(failtext))
-        print("[%2d] Test FAIL%s" % (_count, _RST))
+        print("[%2d] FAIL%s" % (_count, _RST))
+
+        sys.exit(1)
