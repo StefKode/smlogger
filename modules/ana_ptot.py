92339d017f56b48b5766b451ef20ec953f806b59
Stefan Koch <11819754+StefKode@users.noreply.github.com>
2019-12-27 12:40:15 +0100

    unit testing of ptot

--- modules/ana_ptot.py
+++ modules/ana_ptot.py
@@ -7,7 +7,7 @@ class AnaPTot(AnaGeneric):
         self._update_period   = self._opts["update_period"]
         self._redis_key       = self._opts["power_day_key"]
         self._tzone           = datetime.now().astimezone().tzinfo
-        self._force_last_dm   = None
+        self._force_dm        = None
         self._last_dm         = self._get_day_mark()
         self._last_ts         = self._get_ts()
         self._value           = None
@@ -23,21 +23,25 @@ class AnaPTot(AnaGeneric):
         # check if we just crossed the day start (00:00) - if so, reset 
         # the total power capture of the day
         if dm < self._last_dm:
-            self._day_start = p_tot
+            self._day_start = self._inval
         self._last_dm = dm
+
+        if self._day_start is None:
+            self._day_start = self._inval
 
         if (ts - self._last_ts) >= self._update_period:
             pdelta = self._inval - self._day_start
             #convert wh to kwh and send to redis
             self._red.set(self._redis_key, float("{0:.2f}".format(pdelta/1000)))
+            self._value   = pdelta/1000
             self._last_ts = ts
 
 
     # turns a hour + minute into a single numerical value
     # e.g. 23:30 will become the value 2330
     def _get_day_mark(self):
-        if self._force_last_dm is not None:
-            return self._force_last_dm
+        if self._force_dm is not None:
+            return self._force_dm
 
         h = datetime.now(tz=self._tzone).hour
         m = datetime.now(tz=self._tzone).minute
@@ -45,7 +49,15 @@ class AnaPTot(AnaGeneric):
         return marker
 
 
+    def force_dm(self, value):
+        self._force_dm = value
+
+
     def overwrite_last_dm(self, value):
         self._last_dm = value
+
+
+    def overwrite_last_ts(self, value):
+        self._last_ts = value
 
 # vim: set expandtab ts=4:
--- tests/test_ana_ptot.py
+++ tests/test_ana_ptot.py
@@ -22,5 +22,44 @@ try:
 except Exception as e:
     tests.assertEqual(0,1, failtext=str(e))
 
-#tests.announceTest("AnaPTot windowing function - startup")
+tests.announceTest("AnaPTot - check accumulation and redis usage")
+ana.overwrite_last_dm(100)
+ana.force_dm(200)
+ana.overwrite_last_ts(0)
+ana.forceTS(0)
+ana.update(1000)
+tests.assertEqual(red.getLastKey(), None, "redis key not set yet")
+tests.assertEqual(red.getLastVal(), None, "redis val not set yet")
+ana.forceTS(30)
+ana.update(1500)
+tests.assertEqual(red.getLastKey(), "RED_KEY_PTOT")
+tests.assertEqual(red.getLastVal(), 0.5)
+ana.forceTS(60)
+ana.update(1700)
+tests.assertEqual(red.getLastVal(), 0.7)
+ana.forceTS(95)
+ana.update(2700)
+tests.assertEqual(red.getLastVal(), 1.7)
+ana.force_dm(0)
+ana.forceTS(140)
+ana.update(3700)
+tests.assertEqual(red.getLastVal(), 0.0)
+ana.forceTS(170)
+ana.update(4000)
+tests.assertEqual(red.getLastVal(), 0.3)
+ana.forceTS(180)
+ana.update(4100)
+tests.assertEqual(red.getLastVal(), 0.3)
+ana.forceTS(200)
+ana.update(4200)
+tests.assertEqual(red.getLastVal(), 0.5)
+
+tests.announceTest("AnaPTot check day marker")
+from datetime import datetime
+h = datetime.now(tz=datetime.now().astimezone().tzinfo).hour
+m = datetime.now(tz=datetime.now().astimezone().tzinfo).minute
+marker = (h * 100) + m
+tests.log(marker)
+ana.force_dm(None)
+tests.assertEqual(ana._get_day_mark(), marker)
 
