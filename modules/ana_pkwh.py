afaa2c878f303e0578f3408aff58da3f86bcfd79
Stefan Koch <11819754+StefKode@users.noreply.github.com>
2019-12-27 14:18:42 +0100

    add kwh sensor and tests for it

---
+++ modules/ana_pkwh.py
@@ -0,0 +1,40 @@
+
+from datetime import datetime
+from modules.ana_generic import AnaGeneric
+
+class AnaPKwh(AnaGeneric):
+    def _init(self):
+        self._name            = "AnaPKwh"
+        self._update_period   = self._opts["kwh_period"]
+        self._red_key         = self._opts["power_kwh_key"]
+        self._tzone           = datetime.now().astimezone().tzinfo
+        self._last_report_ts  = self._get_ts()
+        self._last_measure_ts = self._last_report_ts
+        self._value           = 0
+        self._sumvalue        = 0
+        
+    def print_opts(self):
+        self._print_opts("timing", "kwh_period", self._update_period)
+        self._print_opts("redis",  "power_kwh_key", self._red_key)
+
+    def _update(self):
+        ts = self._get_ts()
+
+        self._sumvalue += self._inval
+        tdelta = ts - self._last_ts
+        print(tdelta, self._sumvalue)
+
+        if tdelta >= self._update_period:
+            pkwh  = (self._sumvalue * tdelta) / (60 * 60 * 1000)
+            print(pkwh)
+
+            #convert wh to kwh and send to redis
+            self._red.set(self._red_key, float("{0:.2f}".format(pkwh)))
+            self._value   = pkwh
+            self._last_ts = ts
+
+
+    def overwrite_last_ts(self, value):
+        self._last_ts = value
+
+# vim: set expandtab ts=4:
---
+++ tests/test_ana_pkwh.py
@@ -0,0 +1,55 @@
+import sys
+sys.path.append("..")
+
+from modules.ana_pkwh import AnaPKwh
+from rediscon_emu  import RedisCon
+import tests
+import time
+
+red = RedisCon(name="PKWH", host="testhost")
+red.setConMonInterval(1)
+red.connect()
+
+tests.announceTest("AnaPKwh instantiation")
+try:
+    ana = AnaPKwh(redObj=red, 
+                    opts={"power_kwh_key": "RED_KEY_PKWH",
+                          "kwh_period":    30}, 
+                    debug=True)
+    tests.assertEqual(1,1)
+
+except Exception as e:
+    tests.assertEqual(0,1, failtext=str(e))
+
+ana.print_opts()
+tests.announceTest("AnaPKwh - check accumulation and redis usage")
+ana.overwrite_last_ts(0)
+ana.forceTS(0)
+ana.update(1000)
+tests.assertEqual(red.getLastKey(), None, "redis key not set yet")
+tests.assertEqual(red.getLastVal(), None, "redis val not set yet")
+ana.forceTS(30)
+ana.update(1500)
+tests.assertEqual(red.getLastKey(), "RED_KEY_PKWH")
+tests.assertEqual(red.getLastVal(), 0.5)
+ana.forceTS(60)
+ana.update(1700)
+tests.assertEqual(red.getLastVal(), 0.7)
+ana.forceTS(95)
+ana.update(2700)
+tests.assertEqual(red.getLastVal(), 1.7)
+ana.force_dm(0)
+ana.forceTS(140)
+ana.update(3700)
+tests.assertEqual(red.getLastVal(), 0.0)
+ana.forceTS(170)
+ana.update(4000)
+tests.assertEqual(red.getLastVal(), 0.3)
+ana.forceTS(180)
+ana.update(4100)
+tests.assertEqual(red.getLastVal(), 0.3)
+ana.forceTS(200)
+ana.update(4200)
+tests.assertEqual(red.getLastVal(), 0.5)
+
+
